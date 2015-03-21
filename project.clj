3dcbc50a75e86bf478f9cb2c6cb21fb8b66e41cd
dillon <dillonforrest@gmail.com>
2015-03-21 19:33:43 -0400

    update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
-(defproject domshot "0.1.0"
+(defproject domshot "0.2.0"
   :description "Take a snapshot of your dom, a domshot"
   :url "https://github.com/dillonforrest/domshot"
+  :license {:name "MIT"
+            :url "http://opensource.org/licenses/MIT"}
 
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2755"]]
